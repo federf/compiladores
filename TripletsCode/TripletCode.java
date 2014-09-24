2d910196908591c8c8883fbafeb0ca6feb71d469
federf <fede_franco_93@hotmail.com>
2014-09-24 14:02:46 -0300

    INICIO 3er Entrega

---
+++ TripletsCode/TripletCode.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package TripletsCode;
+import ir.ast.*;
+/**
+ *
+ * @author fede
+ */
+public class TripletCode {
+    //primer y segunda direccion
+    Object firstDir, secondDir;
+    //direccion de retorno
+    VarLocation result;
+    //operador
+    Object operator;
+    
+    
+}
